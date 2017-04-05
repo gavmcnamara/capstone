174c65d531e79d76105d9301a0d5936dc5110659
gavmcnamara <gavin.mcnamara01@myhunter.cuny.edu>
2017-04-05 00:10:13 -0400

    updated login page

--- app.py
+++ app.py
@@ -1,24 +1,56 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
- 
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+import hashlib, binascii 
+engine = create_engine('mysql://root:@localhost:3306/shootingla', echo=True)
+
 app = Flask(__name__)
- 
+
+# hashing passowrd
+
+dk = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000)
+binascii.hexlify(dk)
+
+
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return "You are logged in! <a href='/logout'>Logout</a>"
  
+
+
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+    
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
         session['logged_in'] = True
+        return "You are logged in!"
     else:
-        flash('wrong password!')
+        flash('wrong_password!')
+        return "Wrong password!"
+
+
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = True
     return home()
- 
+
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
